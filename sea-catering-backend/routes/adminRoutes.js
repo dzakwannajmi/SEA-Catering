121f27943414221db43fbe668448ec093fe8823d
dzakwannajmi <repmoonasci@gmail.com>
2025-06-27 20:37:05 +0800

    fix admin dashboard call on AdminDashboard.jsx

--- sea-catering-backend/routes/adminRoutes.js
+++ sea-catering-backend/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { verifyToken } = require("../middlewares/authMiddleware");
+const verifyToken = require("../middlewares/authMiddleware");
 const { getAdminData } = require("../controllers/adminController");
 
 router.get("/admin-data", verifyToken, (req, res) => {
